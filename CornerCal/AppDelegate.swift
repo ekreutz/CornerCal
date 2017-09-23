a217a754932c0fa77ae790ea764231677bd24306
Emil Kreutzman <ekreutzman@gmail.com>
2017-09-23 12:39:25 -0700

    Initial Commit

---
+++ CornerCal/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CornerCal
+//
+//  Created by Emil Kreutzman on 23/09/2017.
+//  Copyright © 2017 Emil Kreutzman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
