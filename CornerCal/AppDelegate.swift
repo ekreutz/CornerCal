468beae090a6a27ff25454feb98af90c40e2926f
Emil Kreutzman <ekreutzman@gmail.com>
2017-10-22 12:47:10 -0700

    Default values for settings

--- CornerCal/AppDelegate.swift
+++ CornerCal/AppDelegate.swift
@@ -13,9 +13,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var appController: MainMenuController!
     
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to launch your application        
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        let keys = SettingsKeys()
         
+        // Set some defaults values for this app's settings
+        trySetDefaultValueFor(key: keys.SHOW_SECONDS_KEY, value: false)
+        trySetDefaultValueFor(key: keys.SHOW_DATE_KEY, value: true)
+        trySetDefaultValueFor(key: keys.SHOW_DAY_OF_WEEK_KEY, value: true)
+        trySetDefaultValueFor(key: keys.USE_HOURS_24_KEY, value: true)
+        trySetDefaultValueFor(key: keys.SHOW_AM_PM_KEY, value: true)
+        
+        print("will finish launching")
+    }
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to launch your application
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -25,6 +37,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidUpdate(_ notification: Notification) {
         // Make sure that the internal state is up-to-date
         appController.updateState()
+    }
+    
+    private func defaultsContains(key: String) -> Bool {
+        return UserDefaults.standard.value(forKey: key) != nil
+    }
+    
+    private func trySetDefaultValueFor(key: String, value: Bool) {
+        if (!defaultsContains(key: key)) {
+            print(String(format: "setting default \"%@\"", key))
+            UserDefaults.standard.set(value, forKey: key)
+        }
     }
 
 }
